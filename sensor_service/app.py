ffb044556560b5d80d3d282b69d1763137db0313
Abraham <ja.marinxd@gmail.com>
2025-06-27 12:24:32 -0600

    sensor_services

---
+++ sensor_service/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import requests, time, random
+
+app = Flask(__name__)
+
+def enviar_datos():
+    while True:
+        data = {
+            "barrio": "Centro",
+            "temperatura": round(random.uniform(20, 40), 2),
+            "humedad": round(random.uniform(30, 80), 2),
+            "calidad_aire": round(random.uniform(10, 100), 2)
+        }
+        try:
+            response = requests.post("http://collector_service:5000/datos", json=data)
+            print("Datos enviados:", data)
+        except Exception as e:
+            print("Error enviando datos:", e)
+        time.sleep(5)
+
+if __name__ == "__main__":
+    enviar_datos()
